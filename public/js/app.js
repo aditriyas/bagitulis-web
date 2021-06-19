2eb46e9b07d455dde22c6540549c94b98ba8fd76
benben4567 <benben4567@gmail.com>
2021-06-20 00:26:27 +0700

    initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\bagitulis-laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\bagitulis-laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/v1p3r/Sites/bagitulis/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/v1p3r/Sites/bagitulis/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
