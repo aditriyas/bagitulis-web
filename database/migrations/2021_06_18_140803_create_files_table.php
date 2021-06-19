46e001b1f31cf8e6f8954e07891f68ca892ded42
benben4567 <benben4567@gmail.com>
2021-06-20 00:25:28 +0700

    add author column

--- database/migrations/2021_06_18_140803_create_files_table.php
+++ database/migrations/2021_06_18_140803_create_files_table.php
@@ -16,6 +16,7 @@ class CreateFilesTable extends Migration
         Schema::create('files', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->string('author')->nullable();
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('thumbnail')->nullable();
